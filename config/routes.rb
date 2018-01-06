c500ce61414eca7be1b9d1511fe06167f0027cb1
joeguerra <zorkmerlin>
2018-01-06 14:03:10 -0500

    almost finished, need to retrieve msg from banned user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'check/index'
+  get 'check/create'
+ # get '/search' => 'check/search'
+
+  match "/search" => "check#search", via: :get
+
+  resources :banneds
+  resources :check
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root to: 'banneds#index'
+
 end
